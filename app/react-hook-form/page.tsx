f5381a5bcdcabb11ca8162fd844ed454ddba4c2b
YamazakiYusuke <yusuke38098@gmail.com>
2024-10-05 16:24:27 +0900

    課題1-1

---
+++ app/react-hook-form/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Home() {
+  return (
+    <div className="items-center justify-items-center p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>React Hook Form</h1>
+    </div>
+  );
+}
